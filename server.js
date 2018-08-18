4fc9f1f62f21606a3816d06297a0ffbc4cf4d433
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-08-18 19:28:05 -0400

    FIX

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/store-front'));
+app.use(express.static('./dist'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname,'/dist/store-front/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
