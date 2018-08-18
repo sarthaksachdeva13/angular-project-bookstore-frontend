bcd37bca1b79418a397e5420e8e8e69448f2557a
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-08-18 18:14:52 -0400

    Made edits for API

--- src/app/models/order.ts
+++ src/app/models/order.ts
@@ -2,11 +2,11 @@ import {CartItem} from './cart-item';
 
 export class Order {
 
-	public id: number;
-	public orderDate: string;
-	public shippingDate: string;
-	public shippingMethod: string;
-	public orderStatus: string;
-	public orderTotal: number;
-	public cartItemList: CartItem[];
+    public id: number;
+    public orderDate: string;
+    public shippingDate: string;
+    public shippingMethod: string;
+    public orderStatus: string;
+    public orderTotal: number;
+    public cartItemList: CartItem[];
 }
