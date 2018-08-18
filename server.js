34ebdb81909172a0877ba6bc5ca6098c2de89c9e
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-08-18 19:14:11 -0400

    minor fix

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-
-
-app.use(express.static('./dist/store-front'));
+app.use(express.static('./dist/app'));
 
 app.get('/*', function(req,res) {
-
-    res.sendFile(path.join(__dirname,'/dist/store-front/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
